a06b27d5a2e6f599e27ac5fc03ad3c481ac48c31
emmartinez <emilio_git@outlook.com>
2021-11-03 21:16:36 -0300

    List

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+// el StyleSheet se puede crear en otro archivo y se puede importar en cualquier componente para utilizar los estilos
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Hola, Coder!</Text>
+      <StatusBar style="auto" /> */}
+      {/* En react native a todos los view se les aplica flexbox; en mobile es column y en web es row */}
+      <View style={{padding: 1}}> 
+        {/* Los estilos del componente reciben por prop un objeto js de estilos que contiene las reglas y propiedades css */}
+        <TextInput placeholder='item de lista' style={{borderBottomColor:'black', borderBottomWidth: 1, color: 'violet'}}/>
+        {/* Estos objetos no permiten asignar multiples valores a propiedades CSS, como lo son el padding 0 0 0 0 que recibe up right down y left en una sola linea*/}
+        {/* */}
+        <Button title="ADD" style={{backgroundColor: '#fff'}}/>
+      </View>
+      <View style={{marginTop: 30}}>
+        <View style={styles.lista}>
+          <Text>Item nro 1</Text>
+          <Button title="X"/>
+        </View>
+        <View style={styles.lista}>
+          <Text>Item nro 2</Text>
+          <Button title="X"/>
+        </View>
+        <View style={styles.lista}>
+          <Text>Item nro 3</Text>
+          <Button title="X"/>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +38,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  test: {backgroundColor: '#fff',  borderRadius: 7, backgroundColor: 'black'},
+  lista: {flexDirection: "row"},
 });
